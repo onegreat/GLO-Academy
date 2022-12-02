c0649497c1871c16fd3924109a61ce18f64b0cdb
Onegreat <wangreat@yandex.ru>
2022-12-02 21:28:28 +0300

    Попытка реализовать функцию

--- scripthard.js
+++ scripthard.js
@@ -1,7 +1,23 @@
 let num = 266219;
-let sum = '123';
+let index = 123;
 
+sum = String(index);
 
-for (i = 0; i < sum.length; i++) {
-    console.log("Перемножаем число 266219 на: " + i + " = " + (i * num));
+function sum(index, num) {
+    let i;
+    let
+    for (i = 1; i <= sum.length; i++) {
+        console.log("Перемножаем число 266219 на: " + i + " = " + (i * num));
+        console.log("Возводим число в степень 3 = " + ((i * num) ** 3))
+        console.log("Первые две цифры числа: " + ((i * num) ** 3)splice(0, 2))
+        console.log()
+    }
 }
+/* for (i = 1; i <= sum.length; i++) {
+    console.log("Перемножаем число 266219 на: " + i + " = " + (i * num));
+    console.log("Возводим число в степень 3 = " + ((i * num) ** 3))
+    console.log("Первые две цифры числа: " + ((i * num) ** 3)splice(0, 2))
+    console.log()
+}
+
+*/
