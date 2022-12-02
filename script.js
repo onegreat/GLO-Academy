3b066702348c5e7092c9084fe6488cf74b5e1851
Onegreat <wangreat@yandex.ru>
2022-12-02 19:48:11 +0300

    Сделал конкантинацию

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let title = "Начало обучения";
 let screens = "Простые, сложные, интерактивные";
-let screenPrice = 100;
+let screenPrice = 1000;
 let rollback = 5;
 let fullPrice = 35000;
 let adaptive = true;
@@ -12,5 +12,7 @@ console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 
-console.log(screens.)
+console.log(screens.length);
+
+console.log('Стоимость верстки экрана: ' + screenPrice + "RUB" + " " + "Стоимость верстки: " + fullPrice + "RUB")
 
