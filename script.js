c5bedada69f0cc9f6be01d16b4067931d77b5dd0
Onegreat <wangreat@yandex.ru>
2022-12-03 18:16:45 +0300

    Поправил ошики указанные куратором

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let title = "Начало обучения";
-let screens = "Простые, сложные, интерактивные";
-let screenPrice = 1000;
-let rollback = 5;
-let fullPrice = 35000;
-let adaptive = true;
+const title = "Начало обучения";
+const screens = "Простые, сложные, интерактивные";
+const screenPrice = 1000;
+const rollback = 5;
+const fullPrice = 35000;
+const adaptive = true;
 
 
 console.log('hello');
@@ -16,7 +16,6 @@ console.log(screens.length);
 
 console.log('Стоимость верстки экрана: ' + screenPrice + "RUB" + " " + "Стоимость разработки сайта: " + fullPrice + "RUB")
 
-console.log(screens.toLowerCase());
-console.log(screens.split(", "));
+console.log(screens.toLowerCase(), screens.split(", "));
 
 alert('Привет мир');
