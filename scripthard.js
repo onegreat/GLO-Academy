bdaf0cb87d132ccb40ce55447e5b8eb18e668267
Onegreat <wangreat@yandex.ru>
2022-12-05 21:45:54 +0300

    Переделал задание

--- scripthard.js
+++ scripthard.js
@@ -1,20 +1,11 @@
-const num = 266219;
-let sum = 123;
-
-sum = String(sum); // Для работы цикла меняем значение на стринг
-
-for (i = 1; i <= sum.length; i++) {       // Перемножаем num на каждое число sum.
-    let result;  // Переменная для хранения получвшегося результата при перемножении.
-    let bigInt;  // Переменная для хранения данные возведенных в степень.
-
-    result = i * num;
-    bigInt = result ** 3;
-
-    console.log("Перемножаем число 266219 на: " + i + " = " + (i * num));
-    console.log("Возводим число в степень '3' = " + bigInt)
-    console.log("Первые две цифры полученного числа: " + String(bigInt).slice(0, 2));
-    console.log()
+const num = 266219
+let result = 1
+for (let i = 1; i != 0; i *= 10) {
+    let tmp = Math.trunc(num % (10 * i) / (1 * i))
+    tmp === 0 ? i = 0 : result *= tmp
 }
-
+console.log(result)
+result **= 3
+console.log("Первые две цифры полученного числа: " + String(result).slice(0, 2));
 
 
