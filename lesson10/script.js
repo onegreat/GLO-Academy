fab4450e71f97a3bada6e66630592c97c77e8176
Onegreat <wangreat@yandex.ru>
2022-12-14 21:09:35 +0300

    Удалил рекламу

--- lesson10/script.js
+++ lesson10/script.js
@@ -2,14 +2,13 @@ const ul = document.querySelectorAll('ul')
 const list = document.querySelectorAll('li')
 const block = document.querySelector('body');
 const link = document.querySelectorAll('h2 > a')[4]
+const advertisement = document.querySelector('.adv')
 
 // const list = document.querySelectorAll()
 
 function changeBgImg() {
     block.style.backgroundImage = "url('./image/you-dont-know-js.jpg')";
 }
-
-
 
 ul[0].prepend(list[10])
 ul[0].prepend(list[2])
@@ -22,11 +21,14 @@ ul[0].prepend(list[6])
 ul[0].prepend(list[3])
 ul[0].prepend(list[1])
 ul[0].prepend(list[0])
+
 link.textContent = "Книга 3. this и Прототипы Объектов"
+
+advertisement.remove()
 
 
 changeBgImg();
-
+console.log(advertisement)
 console.log(link)
 console.log(ul)
 console.log(list)
